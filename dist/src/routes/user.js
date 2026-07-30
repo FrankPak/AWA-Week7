16bdd7db0cda8814b8b61a50eb962927f5c246e8
Francois Pakkanen <97538687+FrankPak@users.noreply.github.com>
2026-07-30 19:52:09 +0300

    fix

--- dist/src/routes/user.js
+++ dist/src/routes/user.js
@@ -10,7 +10,7 @@ const express_validator_1 = require("express-validator");
 //import { validateToken } from '../middleware/validateToken'
 const userRouter = (0, express_1.Router)();
 const userList = [];
-userRouter.post('/register', (0, express_validator_1.body)("email").trim().isLength({ min: 3 }).escape(), (0, express_validator_1.body)("password").isLength({ min: 5 }), async (req, res) => {
+userRouter.post('/register', (0, express_validator_1.body)("email").trim().isLength({ min: 3 }).escape(), (0, express_validator_1.body)("password").isLength({ min: 1 }), async (req, res) => {
     const errors = (0, express_validator_1.validationResult)(req);
     if (!errors.isEmpty()) {
         console.log(errors);
@@ -37,7 +37,41 @@ userRouter.post('/register', (0, express_validator_1.body)("email").trim().isLen
         return res.status(500).json({ error: 'Internal Server Error' });
     }
 });
-userRouter.get('/api/user/list', async (req, res) => {
+userRouter.get('/list', async (req, res) => {
+    try {
+        return res.status(200).json(userList);
+    }
+    catch (error) {
+        console.error(`Error during registration: ${error}`);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+userRouter.post('/login', (0, express_validator_1.body)("email").trim().isLength({ min: 3 }).escape(), (0, express_validator_1.body)("password").isLength({ min: 1 }), async (req, res) => {
+    const errors = (0, express_validator_1.validationResult)(req);
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        //const existingUser: IUser | null = await User.findOne({email: req.body.email})
+        const existingUser = userList.find(user => user.email === req.body.email);
+        console.log(existingUser);
+        if (existingUser) {
+            return res.status(403).json({ message: "Email already in use" });
+        }
+        const salt = bcryptjs_1.default.genSaltSync(10);
+        const hash = bcryptjs_1.default.hashSync(req.body.password, salt);
+        const user = {
+            email: req.body.email,
+            password: hash
+        };
+        userList.push(user);
+        return res.status(200).json(user);
+    }
+    catch (error) {
+        console.error(`Error during user login: ${error}`);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 exports.default = userRouter;
 //# sourceMappingURL=user.js.map
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -19,7 +19,7 @@ const userList: IUser[] = []
 
 userRouter.post('/register', 
     body("email").trim().isLength({min: 3}).escape(),
-    body("password").isLength({min: 5}),
+    body("password").isLength({min: 1}),
     async (req: Request, res: Response) => {
         const errors: Result<ValidationError> = validationResult(req)
         if(!errors.isEmpty()) {
@@ -54,8 +54,51 @@ userRouter.post('/register',
     }
 })
 
-userRouter.get('/api/user/list',  async (req: Request, res: Response) => {
+userRouter.get('/list', async (req: Request, res: Response) => {
+    try {
+        return res.status(200).json(userList)
+    } catch (error: any) {
+        console.error(`Error during registration: ${error}`)
+        return res.status(500).json({error: "Internal Server Error"})
+    }
+})
+
+
+userRouter.post('/login',
+    body("email").trim().isLength({min: 3}).escape(),
+    body("password").isLength({min: 1}),
+    async (req: Request, res: Response) => {
+        const errors: Result<ValidationError> = validationResult(req)
+        if(!errors.isEmpty()) {
+            console.log(errors);
+            return res.status(400).json({errors: errors.array()})
+            
+        }
+    try {
+        //const existingUser: IUser | null = await User.findOne({email: req.body.email})
+        const existingUser = userList.find(user => user.email === req.body.email)
+        console.log(existingUser)
+
+        if (existingUser) {
+            return res.status(403).json({message: "Email already in use"})
+        }
+
+        const salt: string = bcrypt.genSaltSync(10)
+        const hash: string = bcrypt.hashSync(req.body.password, salt)
+
+        const user: IUser = {
+            email: req.body.email,
+            password: hash
+        }
+        userList.push(user)
+
+        return res.status(200).json(user)
+
     
+    } catch (error: any){
+        console.error(`Error during user login: ${error}`)
+        return res.status(500).json({ error: 'Internal Server Error' })
+    }
 })
 
 export default userRouter
