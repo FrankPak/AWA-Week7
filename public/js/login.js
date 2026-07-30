2f4e2925f80a6e06f7e312a9efdf30d2b7543dae
Francois Pakkanen <97538687+FrankPak@users.noreply.github.com>
2026-07-30 20:53:53 +0300

    task3?

---
+++ public/js/login.js
@@ -0,0 +1,42 @@
+const initializeLogin = () => {
+    document.getElementById("loginForm").addEventListener("submit", (event) => {
+        fetchData(event)
+    })
+}
+
+const fetchData = async (event) => {
+    event.preventDefault()
+    const formData = {
+        email: event.target.email.value,
+        password: event.target.password.value,
+    }
+    console.log(formData)
+    
+    try {
+        const response = await fetch("http://localhost:3000/api/user/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        if (!response.ok) {
+            document.getElementById("error").innerText = "Error when trying to login. Please try again."
+        } else {
+            const data = await response.json()
+            
+            if(data.token) {
+                localStorage.setItem("auth_token", data.token)
+                window.location.href = "index.html"
+            }
+        }
+
+    } catch (error) {
+        console.error("Error:", error)
+    }
+    document.getElementById("loginForm").reset()
+
+
+}
+
+initializeLogin()
